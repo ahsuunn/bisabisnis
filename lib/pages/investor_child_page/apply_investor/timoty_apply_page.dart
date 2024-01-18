b0b6e25fda92c588a32370105db1116242b7bec7
Ahsan Malik <themalique1910@gmail.com>
2024-01-18 17:03:42 +0700

    added file picker for investor page

--- lib/pages/investor_child_page/apply_investor/timoty_apply_page.dart
+++ lib/pages/investor_child_page/apply_investor/timoty_apply_page.dart
@@ -1,14 +1,34 @@
-import "package:flutter/material.dart";
-import "package:google_fonts/google_fonts.dart";
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
 
 class TimothyApplyPage extends StatefulWidget {
-  const TimothyApplyPage({super.key});
+  const TimothyApplyPage({Key? key});
 
   @override
   State<TimothyApplyPage> createState() => _TimothyApplyPageState();
 }
 
 class _TimothyApplyPageState extends State<TimothyApplyPage> {
+  Map<String, String> fileNames = {
+    'Proposal Bisnis': '',
+    'Daftar Catalog': '',
+    'Business Model Canvas': '',
+    'Ajuan Modal': '',
+  };
+  PlatformFile? newFile;
+
+  Map<String, bool?> fileUploadStatus = {
+    'Proposal Bisnis': null,
+    'Daftar Catalog': null,
+    'Business Model Canvas': null,
+    'Ajuan Modal': null,
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,8 +46,6 @@ class _TimothyApplyPageState extends State<TimothyApplyPage> {
           color: Colors.white,
         ),
       ),
-
-      // body
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 25.0),
         child: Column(
@@ -80,178 +98,153 @@ class _TimothyApplyPageState extends State<TimothyApplyPage> {
               height: 20,
             ),
 
-            // Daftar Catalog
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Proposal Bisnis',
-                      style: GoogleFonts.montserrat(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      'Proposal Bisnis BukaModal.docx',
-                      style: GoogleFonts.montserrat(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.grey,
-                      ),
-                    )
-                  ],
-                ),
-                const Spacer(),
-                const Icon(
-                  Icons.check_circle_sharp,
-                  color: Colors.green,
-                  size: 30,
-                ),
-              ],
-            ),
-            const Divider(
-              color: Color.fromARGB(255, 76, 76, 76),
-            ),
-
-            // Daftar Catalog
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Daftar Catalog',
-                      style: GoogleFonts.montserrat(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      '',
-                      style: GoogleFonts.montserrat(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.grey,
-                      ),
-                    )
-                  ],
-                ),
-                const Spacer(),
-                const Icon(
-                  Icons.add_circle_outline,
-                  color: Colors.grey,
-                  size: 30,
-                ),
-              ],
-            ),
-            const Divider(
-              color: Color.fromARGB(255, 76, 76, 76),
-            ),
-
-            // Business Model Canvas
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Business Model Canvas',
-                      style: GoogleFonts.montserrat(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      '',
-                      style: GoogleFonts.montserrat(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.grey,
-                      ),
-                    )
-                  ],
-                ),
-                const Spacer(),
-                const Icon(
-                  Icons.add_circle_outline,
-                  color: Colors.grey,
-                  size: 30,
-                ),
-              ],
-            ),
-            const Divider(
-              color: Color.fromARGB(255, 76, 76, 76),
-            ),
-
-            // Ajuan Modal
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Ajuan Modal',
-                      style: GoogleFonts.montserrat(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      '',
-                      style: GoogleFonts.montserrat(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.grey,
-                      ),
-                    )
-                  ],
-                ),
-                const Spacer(),
-                const Icon(
-                  Icons.add_circle_outline,
-                  color: Colors.grey,
-                  size: 30,
-                ),
-              ],
-            ),
-            const Divider(
-              color: Color.fromARGB(255, 76, 76, 76),
-            ),
+            // Build each section
+            buildSection('Proposal Bisnis'),
+            buildSection('Daftar Catalog'),
+            buildSection('Business Model Canvas'),
+            buildSection('Ajuan Modal'),
 
             const SizedBox(height: 30),
 
             // Submit
-            Container(
-              height: 40,
-              width: MediaQuery.of(context).size.width * 0.4,
-              decoration: BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Submit',
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.montserrat(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
+            InkWell(
+              onTap: () {},
+              child: Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width * 0.4,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Submit',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.montserrat(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             )
           ],
         ),
       ),
     );
+  }
+
+  //Add a variable to store selected file names for each section
+  Map<String, String?> selectedFileNames = {};
+
+// Build each section
+  Widget buildSection(String sectionTitle) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  sectionTitle,
+                  style: GoogleFonts.montserrat(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+                InkWell(
+                  onTap: () async {
+                    if (newFile != null) {
+                      openFile(newFile!);
+                    }
+                  },
+                  child: Expanded(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      child: Text(
+                        selectedFileNames[sectionTitle] ?? '',
+                        style: GoogleFonts.montserrat(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () async {
+                // Replace this logic with your actual file upload logic
+                final result = await FilePicker.platform.pickFiles(
+                  type: FileType.custom,
+                  allowedExtensions: ['jpg', 'jpeg', 'pdf', 'doc', 'png'],
+                );
+
+                // Open single file
+                if (result == null) return;
+                final file = result.files.first;
+
+                // Update the selected file name
+                selectedFileNames[sectionTitle] = file.name;
+
+                // Update the fileUploadStatus to true
+                setState(() {
+                  fileUploadStatus[sectionTitle] = true;
+                });
+
+                // Save file permanently
+                final newFile = await saveFilePermanently(file);
+
+                print('From Path: ${file.path}');
+                print('To Path: ${newFile.path}');
+
+                // Call openFile after updating selectedFileNames and newFile
+                openFile(file);
+
+                print('Name: ${file.name}');
+                print('Extension: ${file.extension}');
+
+                // uploading file
+              },
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(8),
+              ),
+              child: Icon(
+                fileUploadStatus[sectionTitle] == true
+                    ? Icons.check
+                    : Icons.add,
+                color: fileUploadStatus[sectionTitle] == true
+                    ? Colors.green
+                    : Colors.grey,
+              ),
+            ),
+          ],
+        ),
+        const Divider(
+          color: Color.fromARGB(255, 76, 76, 76),
+        ),
+      ],
+    );
+  }
+
+  Future<File> saveFilePermanently(PlatformFile file) async {
+    final appStorage = await getApplicationDocumentsDirectory();
+    final newFile = File('${appStorage.path}/${file.name}');
+
+    return File(file.path!).copy(newFile.path);
+  }
+
+  void openFile(PlatformFile file) {
+    OpenFile.open(file.path!);
   }
 }
